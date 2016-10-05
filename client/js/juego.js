6bd316f762e7110accd649d52709e9c36939742d
Daniel Espinosa <ec.dani@gmail.com>
2016-10-05 13:11:50 +0200

    Neko! Animaciones!

--- client/js/juego.js
+++ client/js/juego.js
@@ -7,7 +7,7 @@ function preload() {
     game.load.image('ground', 'assets/platform.png');
     game.load.image('ground2', 'assets/platform2.png');
     game.load.image('star', 'assets/star.png');
-    game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
+    game.load.spritesheet('dude', 'assets/neko.png', 32, 32);
     game.load.image('heaven','assets/heaven.png');
 
 }
