f4b703390c467b46f79b9be22e56683c10211506
Daniel Espinosa <ec.dani@gmail.com>
2016-10-28 19:50:37 +0200

    conexion con mongolab

--- node-express.js
+++ node-express.js
@@ -1,29 +1,31 @@
 
-var fs=require("fs");
-var config=JSON.parse(fs.readFileSync("config.json"));
-var host=config.host;
-var port=config.port;
-var exp=require("express");
-var app=exp();
-//var MongoClient= require("mongodb"); // V 3.2.10
-var MongoClient = require('mongodb').MongoClient,
-  f = require('util').format,
-  assert = require('assert');
+var fs = require("fs");
+var MongoClient = require('mongodb').MongoClient;/* // V 3.2.10
+var f = require('util').format;*/
+var assert = require('assert');
+var exp = require("express");
 
 var modelo = require("./servidor/modelo.js");
+
+var config = JSON.parse(fs.readFileSync("config.json"));
+var host = config.host;
+var port = config.port;
+var app = exp();
+
+
 var juego;
 var usuariosCol;
 
 //app.use(app.router);
-app.use(exp.static(__dirname +"/client"));
+app.use(exp.static(__dirname + "/client"));
 
-app.get("/",function(request,response){
-	var contenido=fs.readFileSync("./index.html");
-	response.setHeader("Content-type","text/html");
+app.get("/", function (request, response) {
+	var contenido = fs.readFileSync("./index.html");
+	response.setHeader("Content-type", "text/html");
 	response.send(contenido);
 });
 
-app.get('/crearUsuario/:nombre',function(request,response){
+app.get('/crearUsuario/:nombre', function (request, response) {
 	var usuario = new modelo.Usuario(request.params.nombre);
 	this.juego = new modelo.Juego();
 	this.juego.agregarNivel(new modelo.Nivel("1"));
@@ -31,55 +33,68 @@ app.get('/crearUsuario/:nombre',function(request,response){
 	response.send(this.juego);
 });
 
-app.get('/estadistica',function(request,response){
-	var contenido=fs.readFileSync("./estadistica.html");
-	response.setHeader("Content-type","text/html");
+app.get('/estadistica', function (request, response) {
+	var contenido = fs.readFileSync("./estadistica.html");
+	response.setHeader("Content-type", "text/html");
 	response.send(contenido);
 });
 
 
-console.log("Servidor escuchando en el puerto "+port);
+console.log("Servidor escuchando en el puerto " + port);
 app.listen(process.env.PORT || 1338);
 //app.listen(1338,'localhost');
 
 
 
 
-// Connection URL
-var url = 'mongodb://tprocesos:tprocesos@localhost:27017?authMechanism=SCRAM-SHA-1&authSource=myprojectdb';
-// Use connect method to connect to the Server
-MongoClient.connect(url, function(err, db) {
-  assert.equal(null, err);
-  console.log("Connected correctly to server");
+var url = 'mongodb://tprocesos:tprocesos@ds135577.mlab.com:35577/procesos-gallud';
 
-    db.collection('inserts').insertOne({a:1}, function(err, r) {
-    assert.equal(null, err);
-    assert.equal(1, r.insertedCount);
+MongoClient.connect(url, insertarUsuario);
 
+function insertarUsuario (err, db) {
+	assert.equal(null, err);
 
-  db.close();
-});
+	db.collection('usuarios').insertOne({ nombre: "Pepe", email: "pe@pe.com", clave: "pepe" }, insertarUsuarioCallback );
 
-
-
-var db = new mongo.Db("usuarioscn", new mongo.Server("127.0.0.0","27017",{}));
-
-db.open(function(error){
-	console.log("conectado a Mongo: usuarioscn");
-	db.collection("usuarios", function(err,col){
-		console.log("tenemos la colección");
-		usuariosCol=col;
-	});
-});
-
-insertar({nombre:"Pepe",email:"pe@pe.com",clave:"pepe"});
-
-function insertar(usu){
-	usuariosCol.insert(usu,function(err){
-		if(err){
-			console.log("error");
-		} else {
-			console.log("Nuevo usuario creado");
-		}
-	});
+	function insertarUsuarioCallback(err, r) { // Está anidada, para poder acceder a db.
+		assert.equal(null, err);
+		assert.equal(1, r.insertedCount);
+		db.close();
+	}
 }
+
+
+
+
+
+/*
+function insertarUsuarioCallback (err, r, db) {
+		assert.equal(null, err);
+		assert.equal(1, r.insertedCount);
+		db.close(); // como accedemos a db???
+}*/
+
+
+
+/*
+	var db = new mongo.Db("usuarioscn", new mongo.Server("127.0.0.0", "27017", {}));
+
+	db.open(function (error) {
+		console.log("conectado a Mongo: usuarioscn");
+		db.collection("usuarios", function (err, col) {
+			console.log("tenemos la colección");
+			usuariosCol = col;
+		});
+	});
+
+	insertar({ nombre: "Pepe", email: "pe@pe.com", clave: "pepe" });
+
+	function insertar(usu) {
+		usuariosCol.insert(usu, function (err) {
+			if (err) {
+				console.log("error");
+			} else {
+				console.log("Nuevo usuario creado");
+			}
+		});
+	}*/
