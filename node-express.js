506659015c3a5a0c98d10dba42cab959a42660e7
Daniel Espinosa <ec.dani@gmail.com>
2016-10-29 02:54:20 +0200

    Autenticacion

--- node-express.js
+++ node-express.js
@@ -1,7 +1,7 @@
 
 var fs = require("fs");
-var MongoClient = require('mongodb').MongoClient;/* // V 3.2.10
-var f = require('util').format;*/
+var MongoClient = require('mongodb').MongoClient; // V 3.2.10
+
 var assert = require('assert');
 var exp = require("express");
 var bodyParser = require('body-parser');
@@ -23,28 +23,23 @@ app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 
+/**
+ * Rutas de la app
+ */
+
 app.get("/", function (request, response) {
 	var contenido = fs.readFileSync("./index.html");
 	response.setHeader("Content-type", "text/html");
 	response.send(contenido);
 });
 
-/*app.get('/crearUsuario/:nombre', function (request, response) {
-	var usuario = new modelo.Usuario(request.params.nombre);
-	this.juego = new modelo.Juego();
-	this.juego.agregarNivel(new modelo.Nivel("1"));
-	this.juego.agregarUsuario(usuario);
-	response.send(this.juego);
-});*/
-
 app.post('/crearUsuario', function (request, response) {
-	//console.log(request);
 	var nombre = request.body.nombre;
 	var password = request.body.password;
 
 	var usuario = new modelo.Usuario(nombre);
-	insertarUsuario(usuario,password);
-	
+	insertarUsuario(usuario, password);
+
 	this.juego = new modelo.Juego();
 	this.juego.agregarNivel(new modelo.Nivel("1"));
 	this.juego.agregarUsuario(usuario);
@@ -52,26 +47,21 @@ app.post('/crearUsuario', function (request, response) {
 });
 
 app.post('/autenticarse', function (request, response) {
-	//console.log(request);
 	var nombre = request.body.nombre;
 	var password = request.body.password;
 
-	var doc = cargarUsuario(nombre);
-	console.log(doc);
-	if (doc.password == password) {
-		this.juego = new modelo.Juego();
-		this.juego.agregarNivel(new modelo.Nivel("1"));
-		this.juego.agregarUsuario(usuario);
-		response.send(this.juego);
-	} else {
-		response.status(401).send('Usuario no encontrado.');
-	}
+	cargarUsuario(nombre, callback);
 
-	
-	/*this.juego = new modelo.Juego();
-	this.juego.agregarNivel(new modelo.Nivel("1"));
-	this.juego.agregarUsuario(doc.usuario);
-	response.send(this.juego);*/
+	function callback(doc) {
+		if (doc.password == password) {
+			this.juego = new modelo.Juego();
+			this.juego.agregarNivel(new modelo.Nivel("1"));
+			this.juego.agregarUsuario(doc.usuario);
+			response.send(this.juego);
+		} else {
+			response.status(401).send('Usuario no encontrado.');
+		}
+	}
 });
 
 app.get('/estadistica', function (request, response) {
@@ -81,21 +71,24 @@ app.get('/estadistica', function (request, response) {
 });
 
 
+/**
+ * Lanzar servidor
+ */
+
 console.log("Servidor escuchando en el puerto " + port);
 app.listen(process.env.PORT || 1338);
-//app.listen(1338,'localhost');
 
 
+/**
+ * Funciones de Mongo DB
+ */
 
-
-
-
-function insertarUsuario(usuario,password) {
+function insertarUsuario(usuario, password) {
 	MongoClient.connect(url, conexion);
 	function conexion(err, db) {
 		assert.equal(null, err);
 
-		db.collection('usuarios').insertOne({usuario:usuario,password:password}, callback);
+		db.collection('usuarios').insertOne({ usuario: usuario, password: password }, callback);
 		function callback(err, r) { // Está anidada, para poder acceder a db.
 			assert.equal(null, err);
 			assert.equal(1, r.insertedCount);
@@ -104,57 +97,15 @@ function insertarUsuario(usuario,password) {
 	}
 }
 
-
-function cargarUsuario(nombre) {
-	var resultado = {};
-	
+function cargarUsuario(nombre, callback) {
 	function conexion(err, db) {
 		assert.equal(null, err);
-		//console.log(nombre);
-		function callback(err, r) { // Está anidada, para poder acceder a db.
+		function callback(err, r) {
 			assert.equal(null, err);
-			//console.log(err);
-			//console.log(r);
+			callback(r);
 			db.close();
-			resultado = r;
 		}
-		db.collection('usuarios').findOne({'usuario.nombre':nombre}, callback);
+		db.collection('usuarios').findOne({ 'usuario.nombre': nombre }, callback);
 	}
-	resultado = MongoClient.connect(url, conexion);
-	console.log(resultado);
-	return resultado;
+	MongoClient.connect(url, conexion);
 }
-
-
-
-/*
-function insertarUsuarioCallback (err, r, db) {
-		assert.equal(null, err);
-		assert.equal(1, r.insertedCount);
-		db.close(); // como accedemos a db???
-}*/
-
-
-
-/*
-	var db = new mongo.Db("usuarioscn", new mongo.Server("127.0.0.0", "27017", {}));
-
-	db.open(function (error) {
-		console.log("conectado a Mongo: usuarioscn");
-		db.collection("usuarios", function (err, col) {
-			console.log("tenemos la colección");
-			usuariosCol = col;
-		});
-	});
-
-	insertar({ nombre: "Pepe", email: "pe@pe.com", clave: "pepe" });
-
-	function insertar(usu) {
-		usuariosCol.insert(usu, function (err) {
-			if (err) {
-				console.log("error");
-			} else {
-				console.log("Nuevo usuario creado");
-			}
-		});
-	}*/
