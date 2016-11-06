062e2b0765271d67d84e995a34e720335c468bc6
Daniel Espinosa <ec.dani@gmail.com>
2016-11-07 00:02:47 +0100

    Reconocimiento victoria, sprites, sonido blaster

--- client/components/juego/bootState.js
+++ client/components/juego/bootState.js
@@ -27,6 +27,7 @@ bootState.prototype = {
         game.load.audio('bootMusic', '../components/juego/bgm/02 The Military System.mp3');
         game.load.audio('ingameMusic', '../components/juego/bgm/04 Steel Beast.mp3');
         game.load.audio('endMusic', '../components/juego/bgm/03 Main Theme From Metal Slug.mp3');
+        game.load.audio('blaster', '../components/juego/bgm/blaster.mp3');
     },
     // varios freebies found from google image search
     loadImages: function() {
@@ -36,6 +37,9 @@ bootState.prototype = {
         game.load.image('loading', '../components/juego/img/loading.png');
         game.load.image('brand', '../components/juego/img/logo.png');
         game.load.image('stars', '../components/juego/img/stars.jpg');
+        game.load.image('clouds', '../components/juego/img/metal-slug-3-clouds.png');
+        game.load.image('backgroundMS', '../components/juego/img/backgroundMS.gif');
+        game.load.image('backgroundKU', '../components/juego/img/ku-xlarge.png');
 
     },
 
@@ -71,6 +75,8 @@ bootState.prototype = {
         this.loadingBar = game.make.sprite(game.world.centerX - (387 / 2), 400, "loading");
         this.logo = game.make.sprite(game.world.centerX, 200, 'brand');
 
+
+
         centerGameObjects([this.logo, this.status]);
         function centerGameObjects(objects) {
             objects.forEach(function(object) {
@@ -78,23 +84,30 @@ bootState.prototype = {
             })
         }
 
-        game.add.sprite(0, 0, 'stars');
+        game.add.sprite(0, 0, 'clouds');
         game.add.existing(this.logo).scale.setTo(0.5);
-        game.add.existing(this.loadingBar);
+        //game.add.existing(this.loadingBar);
         game.add.existing(this.status);
         this.load.setPreloadSprite(this.loadingBar);
 
-        this.status.setText('Ready!');
+
+        //var txt = game.add.text(30, 280, 'Start');
+        // so how do we make it clickable?  We have to use .inputEnabled!
+        this.status.inputEnabled = true;
+        // Now every time we click on it, it says "You did it!" in the console!
+        this.status.events.onInputUp.add(function () { game.state.start("juegoState"); });
+
+        this.status.setText('Start!');
         this.addGameStates();
         this.addGameMusic();
 
-        setTimeout(function() {
+        /*setTimeout(function() {
             if (!(game.world === null)) {
                 console.log("ESTO ES LO QUE TIENE GAME:")
                 console.log(game)
                 game.state.start("juegoState");
                 console.log("----NADA MAS----")
             }
-        }, 6000);
+        }, 6000);*/
     }
 };
